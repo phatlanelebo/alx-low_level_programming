0cbbbffeab1eb9476b2efc27d1492f257b256776
lebogang phatlane <lebogangphatlane2@gmail.com>
2022-09-08 10:40:19 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * * Main - Entry point
+ *
+ * *
+ *
+ * * Return: always 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	               puts("");
+
+		                      return (0)
+
+}
+
+

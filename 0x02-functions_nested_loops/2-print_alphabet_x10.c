c243f50f5a40466668ddcf72465d8ee5ee87499a
lebogang phatlane <lebogangphatlane2@gmail.com>
2022-09-14 17:35:25 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_X10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int z;
+	char c;
+
+	for (z = 0; z < 10; ++z)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
